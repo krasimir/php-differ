90d2a95b62ad825d59a6c9ff8efadecba3a252a8
Krasimir Tsonev <krasimir@outset.ws>
2013-06-01 00:28:18 +0300

    Integration of github.com/NV/objectDiff.js

--- js/values.js
+++ js/values.js
@@ -1,7 +1,22 @@
 var ValuesDiffs = (function() {
 
+	var markup = '';
+	var tname = '';
+
 	var get = function(d1, d2, tableName) {
-		console.log(d1, d2, tableName);
+
+		tname = tableName;
+		markup = '';
+
+		// checking of the table exists in both places
+		if(!d1.tables[tname] || !d2.tables[tname]) {
+			markup = 'The table ' + tname + ' is missing in some of the databases.';
+		} else {
+			var diff = objectDiff.diffOwnProperties(d1.tables[tname].values, d2.tables[tname].values);
+			markup = '<pre>' + objectDiff.convertToXMLString(diff) + '</pre>';
+		}
+
+		return markup;
 	}
 
 	return {
